5928c1674082b15bc49112c8833d8bf4257466a0
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-12-10 12:55:09 +1100

    Initial commit of GEL sps code

---
+++ README.md
@@ -0,0 +1,27 @@
+GEL Superpixels
+===============
+
+Source
+------
+https://github.com/jni/gel
+
+Installation from source
+------------------------
+Refer to DEPENDS.txt for a list of dependencies.
+
+GEL may be installed globally using
+
+    $ python setup.py install
+
+or locally using
+
+    $ python setup.py install --prefix=${HOME}
+
+If you prefer, you can use it without installing, by simply adding
+this path to your PYTHONPATH variable and compiling the extensions:
+
+    $ python setup.py build_ext -i
+
+License
+-------
+Please read LICENSE.txt in this directory.
---
+++ gel/_geodesic.py
@@ -0,0 +1,82 @@
+import heapq
+
+import numpy as np
+import scipy.ndimage as nd
+import scipy.spatial.distance as distance
+
+
+def neighbors(coords, shape, connectivity=1):
+    """Return the neighbors of a set of coordinates.
+
+    Parameters
+    ----------
+    coords : array-like
+        The coordinates for which we want neighbor coordinates.
+    shape : array-like
+        The shape of the array including coords. Used to check for borders.
+    connectivity : int (optional), min=1, max=len(shape)
+        The connectivity of the neighborhood.
+
+    Returns
+    -------
+    neighbors : np.ndarray (num_neighbors x len(shape))
+        The coordinates of neighboring array elements.
+    """
+    coords = np.atleast_2d(coords)
+    shape = np.asarray(shape)
+    ndim = len(shape)
+    n_elem = 3 ** ndim
+    footprint = nd.generate_binary_structure(ndim, connectivity)
+    footprint.ravel()[n_elem / 2] = 0
+    neighbors = coords + (np.asarray(footprint.nonzero()).T - np.ones(ndim))
+    not_border = True - ((neighbors < 0).any(axis=1) + 
+                         (neighbors >= shape).any(axis=1))
+    return neighbors[not_border].astype(int)
+
+def geodesic_expansion(labels, image, mode='viscosity', connectivity=1):
+    """Expand the location of labels into a geodesic space defined by image.
+
+    Parameters
+    ----------
+    labels : numpy array (integer type)
+        The initial location of the labels (typically sparse)
+    image : numpy array, dimensions = labels.ndim or labels.ndim + 1
+        The space along which labels must expand. It can either be
+        single-channel (same dimension as `labels`) or multi-channel (one more
+        dimension than `labels`; the last dimension is assumed to be the
+        channels).
+    mode : string (optional)
+        Whether to treat the image values as a `viscosity` (default) or a 
+        `feature`. In the first case, the cost of expanding from pixel `x` to
+        pixel `y` is `image[y]`. In the second, it is `d(image[x], image[y])`.
+    connectivity : int (optional)
+        The connectivity defining neighboring pixels. It is an int between 1
+        and `labels.ndim`, inclusive. (default: 1)
+
+    Returns
+    -------
+    labels : numpy array (integer type)
+        The resulting label field
+    """
+    label_locations = labels.nonzero()
+    initial_labels = labels[label_locations]
+    distance_heap = [(0, coord, label) for coord, label in 
+         zip(np.transpose(label_locations), initial_labels)]
+    distance_array = np.inf * np.ones(labels.shape, float)
+    distance_array[label_locations] = 0
+    if mode == 'viscosity':
+        def dist(img, src, dst): return img[dst]
+    else:
+        def dist(img, src, dst): return distance.euclidean(img[src], img[dst])
+    labels_out = np.zeros_like(labels)
+    while len(distance_heap) > 0:
+        nearest = heapq.heappop(distance_heap)
+        d, loc, lab = nearest
+        if labels_out[loc] == 0:
+            labels_out[loc] = lab
+            distance_array[loc] = d
+            for n in neighbors(loc, labels_out.shape, connectivity):
+                if labels_out[n] == 0:
+                    next_d = d + dist(image, loc, n)
+                    heapq.heappush((next_d, n, lab))
+    return labels
